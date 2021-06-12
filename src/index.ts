11978171617f0cd773d16ce5d7e04901f44caa73
swallowtail62 <swallowtail.a7@gmail.com>
2021-06-12 18:16:52 +0900

    Create mock express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-const main = () => {
-  console.log("foobar");
-};
+import express from "express";
+import { Server } from "http";
 
-main();
+const app = express();
+const http = new Server(app);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const port = Number(process.env.PORT) || 7000;
+http.listen(port);
