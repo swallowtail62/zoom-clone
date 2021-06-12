990b8662199eb7c3a555ac2a5dd952ec128414d6
swallowtail62 <swallowtail.a7@gmail.com>
2021-06-12 19:05:24 +0900

    Render room page html & implement socket io client

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import { v4 as uuidV4 } from "uuid";
 
 const app = express();
 const server = new http.Server(app);
-const io = new Server(server);
+const io = new Server(server, { serveClient: true }); // https://socket.io/docs/v4/server-initialization/#serveClient
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(`roomId: ${roomId}, userId: ${userId}`);
+  });
 });
 
 const port = Number(process.env.PORT) || 7000;
