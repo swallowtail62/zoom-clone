803db530a9acd92ea9a1f328f044da4a4457e137
swallowtail62 <swallowtail.a7@gmail.com>
2021-06-14 02:38:43 +0900

    Establish P2P connection & real-time video call

--- public/script.js
+++ public/script.js
@@ -1,7 +1,48 @@
 const socket = io('/');
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+});
 
-socket.emit('join-room', ROOM_ID, 20);
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
 
-socket.on('user-connected', (usreId) => {
-  console.log(`User connected: ${usreId}`)
-})
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+
+  myPeer.on('call', call => {
+    call.answer(stream);
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream);
+  });
+});
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  video.muted = true;
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on('close', () => {
+    video.remove();
+  })
+}
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+});
